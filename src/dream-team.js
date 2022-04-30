0755afecc252bb0341d572af19b52242e8ded98a
Maxim Trafimov <maximusmiloi@gmail.com>
2022-05-01 02:06:44 +0300

    feat: dream team - done

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,11 +13,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(typeof(members) != 'object' || members === null){ return false};
+  let nameTeam = '';
+  for (i = 0; i < members.length; i++){
+    if(typeof(members[i]) === 'string') {nameTeam += members[i].trim().toUpperCase()[0]}
+    else {nameTeam += ''} 
+  }
+  return nameTeam.split('').sort().join('');
 }
-
 module.exports = {
   createDreamTeam
 };
--- test/dream-team.test.js
+++ test/dream-team.test.js
@@ -1,4 +1,4 @@
-/* const { expect, assert } = require('chai');
+const { expect, assert } = require('chai');
 const { testOptional, checkForNotThrowingErrors, CONSTANTS } = require('../extensions/index.js');
 const { createDreamTeam } = require('../src/dream-team.js');
 
@@ -228,4 +228,3 @@ describe('Dream team', () => {
     });
   });
 });
- */
